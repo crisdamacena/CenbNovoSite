7bfaa52f85a85b0300f86002524239c9fa5c496b
casa <damacena.cris@gmail.com>
2021-03-04 05:02:37 -0300

    Padronizacao_de_Estilo_2
    Toda a estrutura do site e posicionamento dos elementos foi alinhada. O responsivo ainda não está feito, mas as classes e os estilos de cada área foram separados.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,25 @@
 /*!
- * Start Bootstrap - Grayscale v6.0.3 (https://startbootstrap.com/theme/grayscale)
- * Copyright 2013-2020 Start Bootstrap
- * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
- */
-(function($) {
+    * Start Bootstrap - Grayscale v6.0.3 (https://startbootstrap.com/theme/grayscale)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+    */
+    (function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
             location.pathname.replace(/^\//, "") ==
-            this.pathname.replace(/^\//, "") &&
+                this.pathname.replace(/^\//, "") &&
             location.hostname == this.hostname
         ) {
             var target = $(this.hash);
-            target = target.length ?
-                target :
-                $("[name=" + this.hash.slice(1) + "]");
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
             if (target.length) {
-                $("html, body").animate({
+                $("html, body").animate(
+                    {
                         scrollTop: target.offset().top - 70,
                     },
                     1000,
@@ -30,7 +31,7 @@
     });
 
     // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(function() {
+    $(".js-scroll-trigger").click(function () {
         $(".navbar-collapse").collapse("hide");
     });
 
@@ -41,7 +42,7 @@
     });
 
     // Collapse Navbar
-    var navbarCollapse = function() {
+    var navbarCollapse = function () {
         if ($("#mainNav").offset().top > 100) {
             $("#mainNav").addClass("navbar-shrink");
         } else {
@@ -54,8 +55,3 @@
     $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
 
-var myCarousel = document.querySelector('#carrosel')
-var carousel = new bootstrap.Carousel(myCarousel, {
-    interval: 1500,
-    wrap: true
-})
